b4319de084ad5549fb1e84a124faaadff410c3b4
Louis Billaut <ragedfear4875@gmail.com>
2025-07-18 17:35:54 +0800

    add command info constant

--- constants.py
+++ constants.py
@@ -16,3 +16,42 @@ CACHE_TTL = 10800
 
 MINIMUM_CROSSWORD_ID = 21_621
 MAXIMUM_CROSSWORD_ID = 29_747
+
+COMMANDS_INFO = {
+    "start_crossword": [
+        "Start a new cryptic crossword.",
+        []
+    ],
+    "solve_crossword": [
+        "Solve a cryptic crossword clue.",
+        ["clue_number", "word", "direction"]
+    ],
+    "verify_crossword": [
+        "Checks if your cryptic crossword solution is correct for a given clue number.",
+        ["clue_number", "direction", "say_solution"]
+    ],
+    "stop_crossword": [
+        "Stop current cryptic crossword game.",
+        []
+    ],
+    "start_puzzle": [
+        "Start a new cryptic puzzle.",
+        []
+    ],
+    "solve_puzzle": [
+        "Solve a cryptic puzzle clue.",
+        ["word"]
+    ],
+    "stop_puzzle": [
+        "Stop current cryptic puzzle game.",
+        []
+    ],
+    "def": [
+        "Search a word in the dictionary.",
+        ["word"]
+    ],
+    "help": [
+        "Shows a list of commands and their descriptions",
+        []
+    ]
+}
