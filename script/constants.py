5e201c9ee273ac0f691751d4a80daf87241eb977
Louis Billaut <ragedfear4875@gmail.com>
2025-08-21 21:24:58 +0800

    relocate all files

--- script/constants.py
+++ script/constants.py
@@ -6,10 +6,10 @@ TOKEN = os.getenv('ENV_DISCORD_TOKEN')
 TILE_SIZE = 64
 ROWS = 15
 COLS = 15
-IMAGES_PATH = "assets/images/"
-FONT_PATH = "assets/fonts/monogram-extended.ttf"
-BANNER = "assets/bot_images/banner.jpg"
-ICON = "assets/bot_images/icon.jpg"
+IMAGES_PATH = "./assets/images/"
+FONT_PATH = "./assets/fonts/monogram-extended.ttf"
+BANNER = "./assets/bot_images/banner.jpg"
+ICON = "./assets/bot_images/icon.jpg"
 
 CACHE_SIZE = 500
 CACHE_TTL = 10800
